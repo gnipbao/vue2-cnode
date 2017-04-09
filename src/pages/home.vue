a7ecdd53e463c1dace36278bc284bfa45f0640d4
gnipbao <gnipbao@gmail.com>
2017-04-10 00:38:40 +0800

    initial commit

---
+++ src/pages/home.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <img class="index" src="../assets/logo.png">
+        <nv-Home>
+
+        </nv-Home>
+    </div>
+</template>
+<script>
+    import nvHome from '../components/home.vue'
+    export default {
+        data() {
+
+        },
+        components: {
+            nvHome,
+        }
+    };
+</script>
+<style lang="scss">
+    .index {
+        width: 100%;
+        background-color: #fff;
+        margin-top: 40%;
+    }
+</style>
