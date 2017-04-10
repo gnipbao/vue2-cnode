6c0a87df08e59f806dac83cd8f27be33de3c3d03
gnipbao <gnipbao@gmail.com>
2017-04-10 22:54:33 +0800

    add components and routers

---
+++ src/pages/message.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+      <h2>message</h2>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+ 
+</style>
+
+<script>
+    export default {
+        data() {
+            return {
+              
+            }
+        }
+        
+    }
+</script>
---
+++ src/pages/user.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+      <h2>user</h2>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+ 
+</style>
+
+<script>
+    export default {
+        data() {
+            return {
+              
+            }
+        }
+        
+    }
+</script>
